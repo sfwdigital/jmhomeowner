1f32e6635a99229c281018d561d13dc3924feeef
Pete <peteo@sfwresults.com>
2019-07-25 15:00:19 -0400

    file - intro ajax

---
+++ intro.php
@@ -0,0 +1,16 @@
+<div class="content-container">
+  <div class="grid-container">
+    <div class="grid-x">
+      <div class="cell">
+        <img src="http://placehold.it/400x200?text=logo" alt="Johns Manville" />
+      </div>
+    </div>
+    <div class="grid-x">
+      <div class="cell">
+        <h1>FYI: Find Your Insulation</h1>
+        <p class="lead">Home Insulation Guide</p>
+        <a class="button secondary" id="getstarted" title="Get Started">Get Started</a>
+      </div>
+    </div>
+  </div>
+</div>
